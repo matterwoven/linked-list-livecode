62f01e509243452aa8964725cf7ea51e1ac12c9f
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2026-04-28 11:53:16 -0700

    Finished follow along

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,37 @@
+
+import java.util.ArrayList;
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("~linked lists practice~");
+        Node myNode = new Node('w', null);
+        Node otherNode = new Node('x', null);
+        Node coolNode = new Node('e', null);
+
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+
+        ArrayList<Node> nodeArray = new ArrayList<>();
+        nodeArray.add(myNode);
+        nodeArray.add(otherNode);
+        nodeArray.add(coolNode);
+
+        Node current = myNode;
+
+        Boolean switching = true;
+
+        if(switching){
+            while(current != null){
+                System.out.println(current.value);
+                current = current.next;
+            }
+        }
+        else{
+            for(Node n : nodeArray){
+                System.out.println("Node: " + n + " -> \nNode Value: " + n.value);
+            }
+        }
+        
+        //System.out.println("Node Address: " + myNode + " | " + "Node value: " + myNode.value);
     }
 }
